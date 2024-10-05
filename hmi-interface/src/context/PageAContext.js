251516073bbf23384dc25e8915aeac7e6b0bad55
tbaek <roni2006@hanmail.net>
2024-10-05 09:43:06 +0900

    Removed irrelevant files. Added App stylesheet

--- hmi-interface/src/context/PageAContext.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react'
-
-//toggling to false will show two image boxes. Otherwise, four image boxes.
-const PageAContext = createContext({
-    viewMoreToggle: false
-});
-
-export default PageAContext;
--- hmi-interface/src/context/PageSelect.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-
-//toggling to false will show two image boxes. Otherwise, four image boxes.
-const PageSelect = createContext({
-    isPageAView: true
-});
-
-export default PageSelect;
